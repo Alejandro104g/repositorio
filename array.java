244a3fba92a500ed7dba29e3133ea42f6cb62f33
Alejandro104g <alejandroyanezpadial@gmail.com>
2026-04-16 13:28:00 +0200

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array {
+    private int[] numeros;
+    public array() {
+        this.numeros = new int[10];
+    }
+    public void rellenararray(){
+        for (int i = 0; i < this.numeros.length; i++){
+            numeros[i] = (int) (Math.random()*20+1);
+        }
+
+    }
+    public void imprimirarray(){
+        for (int i = 0; i < this.numeros.length; i++){
+            System.out.print(this.numeros[i] + " ");
+        }
+    }
+
+}
